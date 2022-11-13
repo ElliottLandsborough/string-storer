c0907bc7053f7af6c4f01fecbcfea048c36b54b7
Elliott Landsborough <elliott.landsborough@gmail.com>
2022-11-13 23:52:13 +0000

    Some docker and not-working yet mongo

---
+++ api/main.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gorilla/mux"
+	"gopkg.in/mgo.v2"
+)
+
+type Post struct {
+	Title     string    `json:"text" bson:"text"`
+	CreatedAt time.Time `json:"createdAt" bson:"created_at"`
+}
+
+var posts *mgo.Collection
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	// Connect to mongo
+	session, err := mgo.Dial("mongo:27100")
+	if err != nil {
+		log.Println("MongoDB error:")
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+
+	// Get posts collection
+	posts = session.DB("app").C("posts")
+
+	result := []Post{}
+	if err := posts.Find(nil).Sort("-created_at").All(&result); err != nil {
+		responseError(w, err.Error(), http.StatusInternalServerError)
+	} else {
+		responseJSON(w, result)
+	}
+}
+
+func updateHandler(w http.ResponseWriter, r *http.Request) {
+	// Read body
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		responseError(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Read post
+	post := &Post{}
+	err = json.Unmarshal(data, post)
+	if err != nil {
+		responseError(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	post.CreatedAt = time.Now().UTC()
+
+	// Insert new post
+	if err := posts.Insert(post); err != nil {
+		responseError(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	responseJSON(w, post)
+}
+
+func getAllPosts() {
+	// Connect to mongo
+	session, err := mgo.Dial("mongo:27100")
+	if err != nil {
+		log.Println("MongoDB error:")
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+
+	// Get posts collection
+	posts = session.DB("app").C("posts")
+}
+
+func main() {
+	getAllPosts()
+	var wait time.Duration
+	r := mux.NewRouter()
+	r.HandleFunc("/home", homeHandler).Methods("GET")
+	r.HandleFunc("/update", updateHandler).Methods("POST")
+
+	srv := &http.Server{
+		Addr: "0.0.0.0:8080",
+		// Good practice to set timeouts to avoid Slowloris attacks.
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      r, // Pass our instance of gorilla/mux in.
+	}
+
+	// Run our server in a goroutine so that it doesn't block.
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, os.Interrupt)
+
+	// Block until we receive our signal.
+	<-c
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+	// Doesn't block if no connections, but will otherwise wait
+	// until the timeout deadline.
+	srv.Shutdown(ctx)
+	// Optionally, you could run srv.Shutdown in a goroutine and block on
+	// <-ctx.Done() if your application should wait for other services
+	// to finalize based on context cancellation.
+	log.Println("shutting down")
+	os.Exit(0)
+}
+
+func responseError(w http.ResponseWriter, message string, code int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(map[string]string{"error": message})
+}
+
+func responseJSON(w http.ResponseWriter, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(data)
+}
