cd4bac206fa01143ff059c5084284811c8f07530
Elliott Landsborough <elliott.landsborough@gmail.com>
2022-11-14 15:10:13 +0000

    Provision script

---
+++ provision.sh
@@ -0,0 +1,19 @@
+# todo: makefile
+
+# login to ecr
+aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 633322954385.dkr.ecr.eu-west-2.amazonaws.com
+
+# build and push api
+docker build -t string-storer-api ./api
+docker tag string-storer-api:latest 633322954385.dkr.ecr.eu-west-2.amazonaws.com/string-storer-api:latest
+docker push 633322954385.dkr.ecr.eu-west-2.amazonaws.com/string-storer-api:latest
+
+# build and push web
+docker build -t string-storer-web ./web
+docker tag string-storer-web:latest 633322954385.dkr.ecr.eu-west-2.amazonaws.com/string-storer-web:latest
+docker push 633322954385.dkr.ecr.eu-west-2.amazonaws.com/string-storer-web:latest
+
+# Terraform
+cd tf
+terraform init
+terraform apply
