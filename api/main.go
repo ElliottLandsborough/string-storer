5ffdc5aeac48027252c1ae28b67d84cca57ed572
Elliott Landsborough <elliott.landsborough@gmail.com>
2022-11-14 20:46:02 +0000

    Move app back into main

--- api/main.go
+++ api/main.go
@@ -1,6 +1,84 @@
 package main
 
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+)
+
 func main() {
 	app := App{}
 	app.Initialize()
+}
+
+type App struct {
+	Router *mux.Router
+}
+
+func (a *App) Initialize() {
+	createInitialPost()
+
+	var wait time.Duration
+
+	r := mux.NewRouter()
+	r.HandleFunc("/posts", homeHandler).Methods("GET")
+	r.HandleFunc("/posts", updateHandler).Methods("POST")
+
+	a.Router = r
+
+	cor := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:8081", "http://localhost:8080", "http://string-storer-lb-tf-663390881.eu-west-2.elb.amazonaws.com:80"},
+		//AllowCredentials: true,
+	})
+
+	handler := cor.Handler(r)
+
+	srv := &http.Server{
+		Addr: "0.0.0.0:8080",
+		// Good practice to set timeouts to avoid Slowloris attacks.
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      logRequest(handler), // Pass our instance of gorilla/mux in.
+	}
+
+	// Run our server in a goroutine so that it doesn't block.
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, os.Interrupt)
+
+	// Block until we receive our signal.
+	<-c
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+	// Doesn't block if no connections, but will otherwise wait
+	// until the timeout deadline.
+	srv.Shutdown(ctx)
+	// Optionally, you could run srv.Shutdown in a goroutine and block on
+	// <-ctx.Done() if your application should wait for other services
+	// to finalize based on context cancellation.
+	log.Println("shutting down")
+	os.Exit(0)
+}
+
+func logRequest(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
 }
